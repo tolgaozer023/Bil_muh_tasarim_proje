b256a278a1a0b869ad69e86a18859ba7a25e0297
tolgaozer023 <88386789+tolgaozer023@users.noreply.github.com>
2022-01-12 22:28:36 +0300

    Egitici_quiz_uygulamasi
    PROJE EKİBİ :
    
    Celil DEMİR - 15260068
    Tolga ÖZER - 16260004
    Burcu ÇALIŞKAN - 170260021

---
+++ egitici_quiz_uygulamasi/script.js
@@ -0,0 +1,515 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Aşağıdakilerden hangisi temel donanım birimlerinden biri değildir?",
+    answer: "Modem",
+    options: [
+     "Modem",
+     "Ram Bellek",
+     "Anakart",
+     "Sabit disk"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Aşağıdakilerden hangisi doğrudur?",
+    answer: " 1 KB = 1024 Byte",
+    options: [
+        " 1 Kilobyte = 8 Bit",
+        " 1024 MB = 1 KB",
+        " 1 KB = 1024 Byte",
+        " 1 GB = 1024 KB"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Ana belleğin sadece okunabilen, silinemeyen ve değiştirilemeyen kısmına ne ad verilir?",
+    answer: "ROM Bellek",
+    options: [
+      "RAM Bellek",
+      "ROM Bellek",
+      "Hard Disk",
+      "CPU"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Aşağıdakilerden hangisi çıkış birimidir?",
+    answer: "Yazıcı",
+    options: [
+      "Fare",
+      "Klavye",
+      "Optik Disk",
+      "Yazıcı"
+    ]
+  },
+    {
+    numb: 5,
+    question: "2 GB kaç MB eder?",
+    answer: "2048",
+    options: [
+      "1024",
+      "512",
+      "2048",
+      "128"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Aşağıdakilerden hangisi bir programlama dili değildir?",
+    answer: "Windows XP",
+    options: [
+      "Pascal",
+      "Cobol",
+      "Delphi",
+      "Windows XP"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Aşağıdakilerden hangisi uygulama yazılımlarına bir örnek olarak verilebilir?",
+    answer: "Kelime işlem programı",
+    options: [
+      "Makine dili",
+      "Kelime işlem programı",
+      "Programlama dilleri",
+      "İşletim sistemleri"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Hem CD hem DVD okuyup yazabilen aygıt hangisidir?",
+    answer: "DVD-WRITER",
+    options: [
+      "DVD-WRITER",
+      "DVD-ROM",
+      "CD-ROM",
+      "CD-WRITER"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Aşağıdakilerden hangisi depolama birimi değildir?",
+    answer: "Anakart",
+    options: [
+      "Sabit disk",
+      "RAM",
+      "ROM",
+      "Anakart"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Bilgiler geçici olarak hangi bellek üzerinde tutulur?",
+    answer: "RAM",
+    options: [
+      "Memory Card",
+      "Hard disk",
+      "RAM",
+      "ROM"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "Aşağıdakilerden hangisinde bellek birimleri küçükten büyüğe doğru sıralanmıştır?",
+    answer: "Byte- Kilobyte- Megabyte-Gigabyte",
+    options: [
+      "Byte- Kilobyte- Megabyte-Gigabyte",
+      "Byte- Kilobyte- Gigabyte- Megabyte",
+      "Gigabyte- Kilobyte- Byte- Megabyte",
+      "Megabyte- Kilobyte- Byte- Gigabyte"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "Donanım üniteleri arasında veri iletişiminin yapılmasını sağlayan birim hangisidir?",
+    answer: "Anakart",
+    options: [
+      "Harddisk",
+      "Anakart",
+      "ROM",
+      "CPU"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "Bilgisayar bellek birimlerinden 1 byte, kaç bit’ ten oluşur?",
+    answer: "8",
+    options: [
+      "8",
+      "16",
+      "32",
+      "128"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "İç bellek kullanım amacına bağlı olarak kaça ayrılır?",
+    answer: "Ram ve Rom olmak üzere ikiye ayrılır.",
+    options: [
+      "Ram ve Rom olmak üzere ikiye ayrılır.",
+      "Sadece Rom olarak bir tanedir.",
+      "Sadece Ram olarak bir tanedir.",
+      "Ram, Rom, Cps olarak üçe ayrılır."
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Bilgisayarın her türlü elektronik aksamına ne ad verilir?",
+    answer: "Hardware (Donanım)",
+    options: [
+      "CPU (MİB)",
+      "Input Unit ( Giriş birimi)",
+      "Hardware (Donanım)",
+      "Software (Yazılım)"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "Bilgisayarı insanlardan ayıran en önemli özellik nedir?",
+    answer: "İşlem hacmi",
+    options: [
+      "Yorum yeteneği",
+      "Düşünme gücü",
+      "Mantık yürütme",
+      "İşlem hacmi"
+    ]
+  },
+
+  {
+    numb: 17,
+    question: "Aşağıdakilerden hangisi ek donanım birimidir?",
+    answer: "TV kartı",
+    options: [
+      "Ekran kartı",
+      "TV kartı",
+      "Ana kart",
+      "RAM"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "Num Lock tuşunun görevi nedir?",
+    answer: "Sayısal tuşları kilitler ve açar",
+    options: [
+      "Klavyeyi büyük küçük harfe kilitler",
+      "Yön tuşlarının genel adıdır",
+      "Sayısal tuşları kilitler ve açar",
+      "Sekme tuşudur"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "Bilgisayara girilen verilerin işlendiği bölüm hangisidir?",
+    answer: "CPU",
+    options: [
+      "Harddisk",
+      "Flash Bellek",
+      "RAM",
+      "CPU"
+    ]
+  },
+
+  {
+    numb: 20,
+    question: "Ekrandaki en küçük noktalara ne ad verilir?",
+    answer: "Pixel",
+    options: [
+      "Character",
+      "Pixel",
+      "Line",
+      "Column"
+    ]
+  },
+
+  {
+    numb: 21,
+    question: "Aşağıdakilerden hangisi Klavyeyi Büyük harfe kilitler?",
+    answer: "Caps Lock",
+    options: [
+      "Num lock",
+      "Insert",
+      "Caps Lock",
+      "Pause"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "BackSpace tuşunun görevi hangisidir?",
+    answer: "Sola doğru siler",
+    options: [
+      "Sola doğru siler",
+      "Sağa doğru siler",
+      "Karakterler arası boşluk verir",
+      "Kürsörü satırın sonuna götürür"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "Aşağıdakilerden hangisi bilgisayara bilgi girişi için kullanılır?",
+    answer: "Klavye",
+    options: [
+      "Monitör",
+      "Yazıcı",
+      "Plotter",
+      "Klavye"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "Aşağıdakilerden hangisi ana donanım birimi değildir?",
+    answer: "CD Rom",
+    options: [
+      "Ana kart",
+      "Ekran kartı",
+      "CD Rom",
+      "Sabit disk"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "Aşağıdaki depolama araçlarından hangisi kapasitesi en yüksek olandır?",
+    answer: "Sabit Disk",
+    options: [
+      "Zip Disk",
+      "Flash Disk",
+      "Bellek Kartları",
+      "Sabit Disk"
+    ]
+  },
+
+];
+
+
+
+
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+const a=document.querySelector('.icerik');
+window.addEventListener('DOMContentLoaded',function() {
+     var skor=localStorage.getItem('skor');
+     a.innerHTML=`<p>En son yapılan skor: ${skor}</p>`
+      })
+
+
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); 
+}
+
+
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); 
+}
+
+
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); 
+    quiz_box.classList.add("activeQuiz"); 
+    showQuetions(0); 
+    queCounter(1); 
+    startTimer(15);
+    startTimerLine(0); 
+}
+
+let timeValue =  15;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+
+restart_quiz.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz"); 
+    result_box.classList.remove("activeResult");
+    
+    localStorage.setItem('skor',userScore);
+
+    timeValue = 15; 
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuetions(que_count); 
+    queCounter(que_numb); 
+    clearInterval(counter); 
+    clearInterval(counterLine); 
+    startTimer(timeValue); 
+    startTimerLine(widthValue); 
+    timeText.textContent = "Süre"; 
+    next_btn.classList.remove("show"); 
+}
+
+
+quit_quiz.onclick = ()=>{
+    localStorage.setItem('skor',userScore);
+    window.location.reload(); 
+    
+}
+
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){ 
+        que_count++; 
+        que_numb++; 
+        showQuetions(que_count); 
+        queCounter(que_numb); 
+        clearInterval(counter); 
+        clearInterval(counterLine); 
+        startTimer(timeValue); 
+        startTimerLine(widthValue); 
+        timeText.textContent = "Süre"; 
+        next_btn.classList.remove("show"); 
+    }else{
+        clearInterval(counter); 
+        clearInterval(counterLine); 
+        showResult(); 
+    }
+}
+
+
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
+
+    
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; 
+    option_list.innerHTML = option_tag; 
+    
+    const option = option_list.querySelectorAll(".option");
+
+    
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+
+function optionSelected(answer){
+    clearInterval(counter); 
+    clearInterval(counterLine); 
+    let userAns = answer.textContent; 
+    let correcAns = questions[que_count].answer; 
+    const allOptions = option_list.children.length; 
+    
+    if(userAns == correcAns){ 
+        userScore += 1; 
+        answer.classList.add("correct"); 
+        answer.insertAdjacentHTML("beforeend", tickIconTag); 
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect"); 
+        answer.insertAdjacentHTML("beforeend", crossIconTag); 
+        console.log("Wrong Answer");
+
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){ 
+                option_list.children[i].setAttribute("class", "option correct"); 
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled"); 
+    }
+    next_btn.classList.add("show"); 
+}
+
+function showResult(){
+    info_box.classList.remove("activeInfo"); 
+    quiz_box.classList.remove("activeQuiz");
+    result_box.classList.add("activeResult"); 
+    const scoreText = result_box.querySelector(".score_text");
+    if (userScore > 0){ 
+      
+        let scoreTag = '<span>'+ userScore +' soruya doğru cevap verdiniz!</p></span>';
+        scoreText.innerHTML = scoreTag;  
+    }
+}
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time; 
+        time--; 
+        if(time < 9){ 
+            let addZero = timeCount.textContent; 
+            timeCount.textContent = "0" + addZero; 
+        }
+        if(time < 0){ 
+            clearInterval(counter); 
+            timeText.textContent = "Süre bitti!"; 
+            const allOptions = option_list.children.length; 
+            let correcAns = questions[que_count].answer; 
+            for(i=0; i < allOptions; i++){
+                if(option_list.children[i].textContent == correcAns){ 
+                    option_list.children[i].setAttribute("class", "option correct"); 
+                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
+                    console.log("Time Off: Auto selected correct answer.");
+                }
+            }
+            for(i=0; i < allOptions; i++){
+                option_list.children[i].classList.add("disabled"); 
+            }
+            next_btn.classList.add("show"); 
+        }
+    }
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time += 1; 
+        time_line.style.width = time + "px"; 
+        if(time > 549){ 
+            clearInterval(counterLine); 
+        }
+    }
+}
+
+function queCounter(index){
+    
+    let totalQueCounTag = '<span><p>'+ index +'</p> - <p>'+ questions.length +'</p></span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag;  
+}
